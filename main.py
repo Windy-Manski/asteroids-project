d796659d1f442bd60e19aa11910fb9788c2a3bc8
Windy_Manski <jhall3763@gmail.com>
2024-11-01 09:00:44 -0400

    player and imports

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 
 def main():
@@ -8,15 +10,24 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
+        # exit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, color=(0, 0, 0))
+        
+        player.draw(screen) # Player
+
         pygame.display.flip()
-        clock.tick(TICK)
-        dt = clock.tick(TICK) / 1000
+        
+        dt = clock.tick(TICK) / 1000 # FPS control
+        
+
+
           
 if __name__ == "__main__":
     main()
