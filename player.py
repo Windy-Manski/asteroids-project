55442c3bda56b7675cc49b2e93e3cda899e489e3
Windy_Manski <jhall3763@gmail.com>
2024-11-05 09:36:29 -0500

    player w s + move

--- player.py
+++ player.py
@@ -24,9 +24,17 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        inverted_dt = dt * -1
 
         if keys[pygame.K_a]:
-            inverted_dt = dt * -1
             self.rotate(inverted_dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(inverted_dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
