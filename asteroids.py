f30d32691f696b2f2df1e57a68bbc7635ae971d3
Windy_Manski <jhall3763@gmail.com>
2024-11-13 19:06:29 -0500

    destroy p2

--- asteroids.py
+++ asteroids.py
@@ -13,3 +13,14 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            asteriod1 = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS))
+            asteriod2 = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS))
+            asteriod1.velocity = pygame.math.Vector2.rotate(self.velocity, random_angle) * 1.2
+            asteriod2.velocity = pygame.math.Vector2.rotate(self.velocity, -random_angle) * 1.2
--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main():
             for shot in shots.sprites():
                 if CircleShape.collisions(shot, asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         
         screen.fill((0, 0, 0))
         
